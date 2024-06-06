748b9fb717ff90ee1d246ffafd99d51c1e1873c1
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-06-06 13:55:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,153 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fueldecreses() {
+        let fuel = this.fuel - 75;
+        this.fuel;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fueldecreses() {
+        let fuel = this.fuel - 75;
+        this.fuel;
+    }
+}
+let player = await inquirer.prompt({
+    type: "input",
+    name: "name",
+    message: "Please enter your name"
+});
+let opponent = await inquirer.prompt({
+    type: "list",
+    name: "select",
+    message: "Select your opponent",
+    choices: ["Areeba", "Aamir", "Khan"]
+});
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+if (opponent.select === "Areeba") {
+    console.log(`${p1.name} Vs ${o1.name}`);
+    let ask = await inquirer.prompt({
+        type: "list",
+        name: "opt1",
+        message: "Select your option",
+        choices: ["Attack", "Drink", "Dead"]
+    });
+    if (ask.opt1 == "Attack") {
+        let num = Math.floor(Math.random() * 2 + 1);
+        if (num > 0) {
+            p1.fueldecreses();
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+        }
+        if (num <= 0) {
+            o1.fueldecreses();
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+        }
+    }
+    if (ask.opt1 == "Drink") {
+        let num = Math.floor(Math.random() * 2 + 1);
+        if (num > 0) {
+            p1.fueldecreses();
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+        }
+        if (num <= 0) {
+            o1.fueldecreses();
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+        }
+    }
+    if (ask.opt1 == "Dead") {
+        console.log("You loose this game. Try again");
+    }
+}
+//erum
+if (opponent.select === "Aamir") {
+    console.log(`${p1.name} Vs ${o1.name}`);
+    let ask = await inquirer.prompt({
+        type: "list",
+        name: "opt1",
+        message: "Select your option",
+        choices: ["Attack", "Drink", "Dead"]
+    });
+    if (ask.opt1 == "Attack") {
+        let num = Math.floor(Math.random() * 2 + 1);
+        if (num > 0) {
+            p1.fueldecreses();
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+        }
+        if (num <= 0) {
+            o1.fueldecreses();
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+        }
+    }
+    if (ask.opt1 == "Drink") {
+        let num = Math.floor(Math.random() * 2 + 1);
+        if (num > 0) {
+            p1.fueldecreses();
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+        }
+        if (num <= 0) {
+            o1.fueldecreses();
+            console.log(`${o1.name} fuel is ${o1.fuel}`);
+            console.log(`${p1.name} fuel is ${p1.fuel}`);
+        }
+    }
+    if (ask.opt1 == "Dead") {
+        console.log("You loose this game. Try again");
+    }
+    //Sadia
+    if (opponent.select === "Khan") {
+        console.log(`${p1.name} Vs ${o1.name}`);
+        let ask = await inquirer.prompt({
+            type: "list",
+            name: "opt1",
+            message: "Select your option",
+            choices: ["Attack", "Drink", "Dead"]
+        });
+        if (ask.opt1 == "Attack") {
+            let num = Math.floor(Math.random() * 2 + 1);
+            if (num > 0) {
+                p1.fueldecreses();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`);
+            }
+            if (num <= 0) {
+                o1.fueldecreses();
+                console.log(`${o1.name} fuel is ${o1.fuel}`);
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+            }
+        }
+        if (ask.opt1 == "Drink") {
+            let num = Math.floor(Math.random() * 2 + 1);
+            if (num > 0) {
+                p1.fueldecreses();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`);
+            }
+            if (num <= 0) {
+                o1.fueldecreses();
+                console.log(`${o1.name} fuel is ${o1.fuel}`);
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+            }
+        }
+        if (ask.opt1 == "Dead") {
+            console.log("You loose this game. Try again");
+        }
+    }
+}
